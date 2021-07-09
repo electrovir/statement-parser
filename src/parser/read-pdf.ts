26edd3cd994047def67358c7b365a7daf643c8a0
electrovir <electrovir@users.noreply.github.com>
2021-07-09 09:06:01 -0600

    use fx-extra for reading

--- src/parser/read-pdf.ts
+++ src/parser/read-pdf.ts
@@ -1,19 +1,24 @@
-import {existsSync} from 'fs';
+import {existsSync} from 'fs-extra';
 import {readPdfText} from 'pdf-text-reader';
 import {getDocument, VerbosityLevel} from 'pdfjs-dist/legacy/build/pdf';
 import {DocumentInitParameters, PDFDocumentProxy} from 'pdfjs-dist/types/display/api';
 
 export async function readPdf(path: string): Promise<string[][]> {
-    if (!existsSync(path)) {
-        throw new Error(`File "${path}" does not exist`);
-    }
+    checkThatPdfExists(path);
     return (await readPdfText(createSource(path))).map((page) => page.lines);
 }
 
 export async function getPdfDocument(path: string): Promise<PDFDocumentProxy> {
+    checkThatPdfExists(path);
     return await getDocument(createSource(path)).promise;
 }
 
 function createSource(path: string): DocumentInitParameters {
     return {url: path, verbosity: VerbosityLevel.ERRORS};
+}
+
+function checkThatPdfExists(filePath: string): void {
+    if (!existsSync(filePath)) {
+        throw new Error(`File "${filePath}" does not exist`);
+    }
 }
