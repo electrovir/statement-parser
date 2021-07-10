e6c2e87d122fa81025f6b62d14529f91d10fa5c3
electrovir <electrovir@users.noreply.github.com>
2021-07-10 08:51:56 -0600

    create function to add flags to RegExp objects

--- src/augments/regexp.test.ts
+++ src/augments/regexp.test.ts
@@ -1,18 +1,39 @@
 import {testGroup} from 'test-vir';
-import {deDupeFlags} from './regexp';
+import {addRegExpFlags, deDupeRegExFlags} from './regexp';
 
 testGroup({
-    description: deDupeFlags.name,
+    description: deDupeRegExFlags.name,
     tests: (runTest) => {
         runTest({
             description: 'deDupes consecutive flags',
             expect: 'ig',
-            test: () => deDupeFlags('iIiIgGgG'),
+            test: () => deDupeRegExFlags('iIiIgGgG'),
         });
         runTest({
             description: 'deDupes nonconsecutive flags',
             expect: 'ig',
-            test: () => deDupeFlags('igIgI'),
+            test: () => deDupeRegExFlags('igIgI'),
+        });
+    },
+});
+
+testGroup({
+    description: addRegExpFlags.name,
+    tests: (runTest) => {
+        runTest({
+            description: 'adds flags to a RegExp',
+            expect: 'i',
+            test: () => addRegExpFlags(/nothing to see here/, 'i').flags,
+        });
+        runTest({
+            description: 'does not duplicate flags',
+            expect: 'i',
+            test: () => addRegExpFlags(/nothing to see here/i, 'i').flags,
+        });
+        runTest({
+            description: 'preserves original flags',
+            expect: 'gi',
+            test: () => addRegExpFlags(/nothing to see here/g, 'i').flags,
         });
     },
 });
