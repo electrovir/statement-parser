31c10f4b7b9175442ec10c2a7621b2173096239e
electrovir <electrovir@users.noreply.github.com>
2021-07-17 09:26:39 -0600

    move sample files around

--- src/repo-paths.ts
+++ src/repo-paths.ts
@@ -1,9 +1,12 @@
 import {join} from 'path';
 
 export const repoRootDir = __dirname.replace(/(?:src|dist).*/, '');
-export const sampleFileDir = join(repoRootDir, 'sample-files');
-export const dummyPdfPath = join(sampleFileDir, 'dummy.pdf');
-export const downloadsDir = join(repoRootDir, 'downloads');
 
-export const temp_sanitizerRawTestFilePath = join(downloadsDir, 'last-raw-text-for-sanitizer.txt');
-export const temp_sanitizerSanitizedTextFilePath = join(downloadsDir, 'last-sanitized-text.txt');
+const filesDir = join(repoRootDir, 'files');
+
+export const sampleFileDir = join(filesDir, 'sample-files');
+export const dummyPdfPath = join(sampleFileDir, 'dummy.pdf');
+
+export const tempOutputDir = join(filesDir, 'temp-output');
+export const temp_sanitizerRawTestFilePath = join(tempOutputDir, 'last-raw-text-for-sanitizer.txt');
+export const temp_sanitizerSanitizedTextFilePath = join(tempOutputDir, 'last-sanitized-text.txt');
