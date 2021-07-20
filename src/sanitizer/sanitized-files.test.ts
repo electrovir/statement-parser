44522e0eeafab9660f682a33bea5d35078081197
electrovir <electrovir@users.noreply.github.com>
2021-07-20 07:38:15 -0600

    move sanitized files test into sanitizer
    cause that makes way more sense

--- src/sanitizer/sanitized-files.test.ts
+++ src/sanitizer/sanitized-files.test.ts
@@ -1,7 +1,7 @@
 import {join} from 'path';
 import {testGroup} from 'test-vir';
-import {getAllRecursiveFiles, sanitizedFilesDir} from './repo-paths';
-import {createSanitizedTestInput} from './sanitizer/sanitized-test';
+import {getAllRecursiveFiles, sanitizedFilesDir} from '../repo-paths';
+import {createSanitizedTestInput} from './sanitized-test';
 
 async function getAllSanitizedFiles(): Promise<string[]> {
     return Array.from(await getAllRecursiveFiles(sanitizedFilesDir)).map((subPath) =>
